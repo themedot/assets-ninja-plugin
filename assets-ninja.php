08967317bc938fdd7fe0cf01a0aa3535fef1ee70
Mahadi001 <sadat.himel@gmail.com>
2021-06-23 19:12:14 +0600

    updated

--- assets-ninja.php
+++ assets-ninja.php
@@ -26,18 +26,21 @@ class AssetsNinjs {
 
         add_action( 'plugins_loaded', [$this, 'load_textdomain'] );
         add_action( 'wp_enqueue_scripts', [$this, 'load_front_assets'] );
-        add_action( 'admin_enqueue_scripts',[$this,'load_admin_assets'] );
+        add_action( 'admin_enqueue_scripts', [$this, 'load_admin_assets'] );
     }
 
-    function load_admin_assets(){
-        wp_enqueue_script('asn-admin-js',ASN_ASSETS_ADMIN_DIR. "js/admin.js",['jquery'],$this->version, true);
+    function load_admin_assets( $hook ) {
+        $_hook = get_current_screen();
+        if ( 'edit.php' == $hook && 'page' == $_hook->post_type ) {
+            wp_enqueue_script( 'asn-admin-js', ASN_ASSETS_ADMIN_DIR . "js/admin.js", ['jquery'], $this->version, true );
+        }
     }
 
     function load_front_assets() {
         wp_enqueue_style( 'asn-main-css', ASN_ASSETS_PUBLIC_DIR . "css/main.css", null, $this->version );
-        wp_enqueue_script( 'asn-main-js', ASN_ASSETS_PUBLIC_DIR . "js/main.js", [ 'jquery' ], $this->version, true );
-        wp_enqueue_script( 'asn-another-js', ASN_ASSETS_PUBLIC_DIR . "js/another.js", [ 'jquery' ], $this->version, true );
-        wp_enqueue_script( 'asn-more-js', ASN_ASSETS_PUBLIC_DIR . "js/more.js", [ 'jquery' ], $this->version, true );
+        wp_enqueue_script( 'asn-main-js', ASN_ASSETS_PUBLIC_DIR . "js/main.js", ['jquery'], $this->version, true );
+        wp_enqueue_script( 'asn-another-js', ASN_ASSETS_PUBLIC_DIR . "js/another.js", ['jquery'], $this->version, true );
+        wp_enqueue_script( 'asn-more-js', ASN_ASSETS_PUBLIC_DIR . "js/more.js", ['jquery'], $this->version, true );
 
         $data = [
             'name' => 'Sadat Himel',
